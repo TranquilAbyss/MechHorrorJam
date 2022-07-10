2e07abd21256c0f96efb1129f12764541b0cd177
DESKTOP-CT9BLFA\georg <george@musegames.com>
2022-07-10 12:16:14 -0400

    -add cockpit - example hiding tenticle animation - update texture in enviroment

---
+++ Assets/Scripts/AnimationTrigger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTrigger : MonoBehaviour
+{
+    public Animation animation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        animation.Play();
+        gameObject.GetComponent<Collider>().enabled = false;
+    }
+}
