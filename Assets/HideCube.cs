fd2f0128bb54c5b0f1f5da5123ebf50b0da88eb7
TranquilAbyss <ms5938@gmail.com>
2022-07-18 17:01:39 -0400

    Started Camera visablity and Static

---
+++ Assets/HideCube.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Visibility))]
+public class HideCube : MonoBehaviour
+{
+    Visibility vis;
+
+    public Image headCameraStatic;
+    public Image leftCameraStatic;
+    public Image rightCameraStatic;
+    public Image backCameraStatic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vis = GetComponent<Visibility>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Camera camera in vis.OnCameras())
+        {
+
+            if (camera.gameObject.name != "PlayerCamera")
+            {
+
+                if (camera.gameObject.name == "HeadCamera")
+                {
+                    headCameraStatic.enabled = true;
+                }
+                if (camera.gameObject.name == "LeftCam")
+                {
+                    leftCameraStatic.enabled = true;
+                }
+                if (camera.gameObject.name == "RightCam")
+                {
+                    rightCameraStatic.enabled = true;
+                }
+                if (camera.gameObject.name == "BackCam")
+                {
+                    backCameraStatic.enabled = true;
+                }
+            }
+        }
+
+        foreach (Camera camera in vis.NotOnCameras())
+        {
+            if (camera.gameObject.name != "PlayerCamera")
+            {
+                if (camera.gameObject.name == "HeadCamera")
+                {
+                    headCameraStatic.enabled = false;
+                }
+                if (camera.gameObject.name == "LeftCam")
+                {
+                    leftCameraStatic.enabled = false;
+                }
+                if (camera.gameObject.name == "RightCam")
+                {
+                    rightCameraStatic.enabled = false;
+                }
+                if (camera.gameObject.name == "BackCam")
+                {
+                    backCameraStatic.enabled = false;
+                }
+            }
+        }
+
+    }
+}
---
+++ Assets/Visibility.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Visibility : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public List<Camera> OnCameras()
+    {
+        List<Camera> cameras = new List<Camera>();
+        RaycastHit hit;
+        foreach (Camera camera in Camera.allCameras)
+        {
+            if (IsInCameraView(camera, gameObject))
+            {
+                Vector3 dir = (transform.position - camera.transform.position).normalized;
+                Ray ray = new Ray(camera.transform.position, dir);
+                Physics.Raycast(ray, out hit);
+                Debug.DrawRay(camera.transform.position, dir);
+
+                if (hit.collider.gameObject.GetHashCode() == gameObject.GetHashCode())
+                {
+                    cameras.Add(camera);
+                }
+            }
+        }
+
+        return cameras;
+    }
+
+    public List<Camera> NotOnCameras()
+    {
+        List<Camera> cameras = new List<Camera>();
+        RaycastHit hit;
+        foreach (Camera camera in Camera.allCameras)
+        {
+            if (IsInCameraView(camera, gameObject))
+            {
+                Vector3 dir = (transform.position - camera.transform.position).normalized;
+                Ray ray = new Ray(camera.transform.position, dir);
+                Physics.Raycast(ray, out hit);
+                Debug.DrawRay(camera.transform.position, dir);
+
+                if (hit.collider.gameObject.GetHashCode() != gameObject.GetHashCode())
+                {
+                    cameras.Add(camera);
+                }
+            }
+            else
+            {
+                cameras.Add(camera);
+            }
+        }
+
+        return cameras;
+    }
+
+    public bool IsVisable()
+    {
+        bool visible = false;
+        RaycastHit hit;
+        foreach (Camera camera in Camera.allCameras)
+        {
+            if (IsInCameraView(camera, gameObject))
+            {
+                Vector3 dir = (transform.position - camera.transform.position).normalized;
+                Ray ray = new Ray(camera.transform.position, dir);
+                Physics.Raycast(ray, out hit);
+                Debug.DrawRay(camera.transform.position, dir);
+
+                if (hit.collider.gameObject.GetHashCode() == gameObject.GetHashCode())
+                {
+                    visible = true;
+                }
+            }
+        }
+
+        return visible;
+    }
+
+    bool IsInCameraView(Camera c, GameObject go)
+    {
+        var planes = GeometryUtility.CalculateFrustumPlanes(c);
+        var point = go.transform.position;
+        foreach (var plane in planes)
+        {
+            if (plane.GetDistanceToPoint(point) < 0)
+                return false;
+        }
+        return true;
+    }
+}
