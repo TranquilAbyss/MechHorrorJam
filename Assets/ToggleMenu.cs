189149c12a0902be4e7df1ee0e33fca521cf7224
TranquilAbyss <ms5938@gmail.com>
2022-07-20 16:55:30 -0400

    Added Continue Button

--- Assets/ToggleMenu.cs
+++ Assets/ToggleMenu.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ToggleMenu : MonoBehaviour
 {
     public MechControls mechControls;
     public FPSCameraPivot upperBodyPivot;
     public GameObject MenuGraphics;
+    public Text playButtonText;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,7 @@ public class ToggleMenu : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            playButtonText.text = "Continue";
             mechControls.enabled = !mechControls.enabled;
             upperBodyPivot.toggleMouseLock = !upperBodyPivot.toggleMouseLock;
             MenuGraphics.SetActive(!MenuGraphics.activeSelf);
